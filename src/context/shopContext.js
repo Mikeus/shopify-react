237f1121e16b3cc32905bd12149af63eead210d9
Michal Ziembicki <vipmikeus@gmail.com>
2022-03-29 17:57:05 -0400

    updated shop conf api

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import Client from 'shopify-buy'
 
+const SHOP_URL = process.env.development.local.SHOP_URL
+const STOREFRONT_API_ACCESS_TOKEN =
+  process.env.development.local.STOREFRONT_API_ACCESS_TOKEN
+
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain: 'your-shop-name.myshopify.com',
-  storefrontAccessToken: 'your-storefront-access-token',
+  domain: SHOP_URL,
+  storefrontAccessToken: STOREFRONT_API_ACCESS_TOKEN,
 })
 
 class ShopProvider extends Component {
